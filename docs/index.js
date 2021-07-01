4a5a4a4ee59ab9e49e33af693d77d93bf95c5c44
turner <csusbdt@gmail.com>
2021-07-01 15:09:31 -0700

    X

--- docs/index.js
+++ docs/index.js
@@ -32,15 +32,38 @@ fullscreen_touch.stops(fullscreen_loop);
 splash_fullscreen_touch.starts(s_tick, g.request_fullscreen);
 splash_windowed_touch.starts(s_tick, start_fullscreen);
 fullscreen_touch.starts(s_tick, g.request_fullscreen);
-function start_splash() {
+function start_splash(...start_set) {
 	if (g.fullscreen_enabled()) {
 		splash_loop.start();
 		splash_fullscreen_touch.start();
 		splash_windowed_touch.start();
+		start_set.forEach(o => {
+			splash_fullscreen_touch.starts(o);
+			splash_windowed_touch.starts(o);	
+		});
+	} else {
+		start_set.forEach(o => o.start());
 	}
 }
 
 //#endregion
+
+const exit_loop_frames    = g.frames([i_exit]);
+const exit_opening_frames = g.frames([i_exit_2, i_exit_1]);
+const exit_closing_frames = g.frames([i_exit_1, i_exit_2]);
+
+const exit_loop        = g.loop(exit_loop_frames, 10);
+
+const exit_opening     = g.once(exit_opening_frames, 10);
+const exit_closing     = g.once(exit_closing_frames, 10);
+
+const exit_touch       = g.touch(g.rect(0, 0, 222, 126));
+
+exit_closing.starts(() => window.location.assign('home.html'));
+
+exit_touch.stops(exit_loop);
+
+exit_touch.starts(s_blop, exit_closing);
 
 const red_loop_frames        = g.frames([i_red]);
 const red_opening_frames     = g.frames([i_red_2, i_red_1]);
@@ -51,11 +74,7 @@ const red_loop        = g.loop(red_loop_frames, 10);
 const red_opening     = g.once(red_opening_frames, 10);
 const red_closing     = g.once(red_closing_frames, 10);
 
-const red_touch       = g.touch(g.rect(100, 100, 700, 325));
-
-const touches = () => {
-	red_touch.start();
-};
+const red_touch       = g.touch(g.rect(340, 230, 933, 453));
 
 red_closing.starts(() => window.location.replace('red/'));
 
@@ -63,8 +82,11 @@ red_touch.stops(red_loop);
 
 red_touch.starts(s_blop, red_closing);
 
+const touches = () => {
+	exit_touch.start();
+	red_touch.start();
+};
+
 window.addEventListener('load', e => {
-	start_splash();
-	red_loop.start();
-	touches();
+	start_splash(exit_loop, red_loop, touches);
 });

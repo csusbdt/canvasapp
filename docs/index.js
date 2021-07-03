4b65b5a4af3332ddd2dfb668a33e9eb089dc2431
turner <csusbdt@gmail.com>
2021-07-02 22:02:44 -0700

    X

--- docs/index.js
+++ docs/index.js
@@ -5,12 +5,11 @@ import start_exit   from './exit.js';
 document.title = "canvas app example";
 
 const red_loop_frames    = g.frames([i_red]);
-const red_opening_frames = g.frames([i_red_2, i_red_1]);
-const red_closing_frames = g.frames([i_red_1, i_red_2]);
+const red_opening_frames = g.frames([i_red_2, i_red_1], .088);
+const red_closing_frames = g.frames([i_red_1, i_red_2], .088);
 const red_loop    = g.loop(red_loop_frames   , 10);
 const red_opening = g.once(red_opening_frames, 10);
 const red_closing = g.once(red_closing_frames, 10);
-const red_delay   = g.delay(.01);
 const red_replace = () => window.location.replace('red/');
 const red_blop    = g.sound(s_blop);
 const red_touch   = g.touch(g.rect(340, 230, 933, 453));
@@ -18,7 +17,6 @@ const red_touch   = g.touch(g.rect(340, 230, 933, 453));
 red_opening.starts(red_loop, red_touch);
 red_touch.stops(red_loop).starts(red_blop, red_closing);
 red_blop.starts(red_replace);
-red_closing.starts(red_delay);
 
 const touches = () => {
 	red_touch.start();
--- docs/red/index.js
+++ docs/red/index.js
@@ -5,12 +5,11 @@ import start_exit   from './../exit.js';
 document.title = "red";
 
 const blue_loop_frames    = g.frames([i_blue]);
-const blue_opening_frames = g.frames([i_blue_2, i_blue_1]);
-const blue_closing_frames = g.frames([i_blue_1, i_blue_2]);
+const blue_opening_frames = g.frames([i_blue_2, i_blue_1], .088);
+const blue_closing_frames = g.frames([i_blue_1, i_blue_2], .088);
 const blue_loop    = g.loop(blue_loop_frames   , 10);
 const blue_opening = g.once(blue_opening_frames, 10);
 const blue_closing = g.once(blue_closing_frames, 10);
-const blue_delay   = g.delay(.01);
 const blue_replace = () => window.location.replace('../');
 const blue_blop    = g.sound(s_blop);
 const blue_touch   = g.touch(g.circle(585, 330, 190));
@@ -18,7 +17,6 @@ const blue_touch   = g.touch(g.circle(585, 330, 190));
 blue_opening.starts(blue_loop, blue_touch);
 blue_touch.stops(blue_loop).starts(blue_blop, blue_closing);
 blue_blop.starts(blue_replace);
-blue_closing.starts(blue_delay);
 
 const touches = () => {
 	blue_touch.start();
